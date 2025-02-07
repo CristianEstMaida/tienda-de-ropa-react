7011987e34a328c72a2dd97ba1ac5ebcb35dbcda
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2025-02-07 17:28:57 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,28 @@ import { Link } from 'react-router-dom'
 import CartWidget from './CartWidget'
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-        <Link to="/" className="logo"><h1>Tienda de ropa</h1></Link>
-        <ul className="menu">
-            {/* <li><Link className="menu-link" to="/">Inicio</Link></li> */}
-            <li><Link className="menu-link" to="/productos">Productos</Link></li>
-            <li><Link className="menu-link" to="/productos/medias">Medias</Link></li>
-            <li><Link className="menu-link" to="/productos/pantalones">Pantalones</Link></li>
-            <li><Link className="menu-link" to="/productos/remeras">Remeras</Link></li>
-            <li><Link className="menu-link" to="/productos/abrigos">Buzos</Link></li>
-            <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
-            <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
-            <li><CartWidget /></li>
-        </ul>
-    </nav>
+  return (    
+      <aside>
+        <button className="close-menu" id="close-menu">
+          <i className="bi bi-x"></i>
+        </button>
+        <header className="navbar">
+         <Link to="/" className="logo"><h1>Tienda de ropa</h1></Link>
+        </header>
+        <nav>
+          <ul className="menu">
+              {/* <li><Link className="menu-link" to="/">Inicio</Link></li> */}
+              <li><Link className="menu-link boton-categoria" to="/productos">Productos</Link></li>
+              <li><Link className="menu-link boton-categoria" to="/productos/medias">Medias</Link></li>
+              <li><Link className="menu-link boton-categoria" to="/productos/pantalones">Pantalones</Link></li>
+              <li><Link className="menu-link boton-categoria" to="/productos/remeras">Remeras</Link></li>
+              <li><Link className="menu-link boton-categoria" to="/productos/abrigos">Buzos</Link></li>
+              <li><Link className="menu-link boton-categoria" to="/nosotros">Nosotros</Link></li>
+              <li><Link className="menu-link boton-categoria" to="/contacto">Contacto</Link></li>
+              <li><CartWidget /></li>
+          </ul>
+        </nav>
+      </aside>
   )
 }
 
