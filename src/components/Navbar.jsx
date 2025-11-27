dfd9b1ed4213843171eb3e8459ce5f28375be64a
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-26 22:24:25 -0300

    Replace menu list with NavLinks component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,17 +13,7 @@ const Navbar = () => {
          <Link to="/" className="logo"><h1>Tienda de ropa</h1></Link>
         
         <nav>
-          <ul className="menu">
-              
-              <li><Link className="menu-link boton-categoria" to="/productos">Productos</Link></li>
-              <li><Link className="menu-link boton-categoria" to="/productos/medias">Medias</Link></li>
-              <li><Link className="menu-link boton-categoria" to="/productos/pantalones">Pantalones</Link></li>
-              <li><Link className="menu-link boton-categoria" to="/productos/remeras">Remeras</Link></li>
-              <li><Link className="menu-link boton-categoria" to="/productos/abrigos">Buzos</Link></li>
-              <li><Link className="menu-link boton-categoria" to="/nosotros">Nosotros</Link></li>
-              <li><Link className="menu-link boton-categoria" to="/contacto">Contacto</Link></li>
-              <li><CartWidget /></li>
-          </ul>
+          <NavLinks />
         </nav>
     </header>
     <AsideMenu />
