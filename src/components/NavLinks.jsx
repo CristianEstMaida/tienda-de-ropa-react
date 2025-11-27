85976a461da1ee9068863edaea2f421b66c59ad2
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-26 22:52:59 -0300

    Add closeMenu prop to NavLinks component

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CartWidget from './CartWidget';
 
-const NavLinks = () => {
+const NavLinks = ({ closeMenu }) => { 
   return (
     <ul className="menu">
-      <li><Link className="menu-link boton-categoria" to="/productos">Productos</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/medias">Medias</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/pantalones">Pantalones</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/remeras">Remeras</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/abrigos">Buzos</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/nosotros">Nosotros</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/contacto">Contacto</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos" onClick={closeMenu}>Productos</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/medias" onClick={closeMenu}>Medias</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/pantalones" onClick={closeMenu}>Pantalones</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/remeras" onClick={closeMenu}>Remeras</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/abrigos" onClick={closeMenu}>Buzos</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/nosotros" onClick={closeMenu}>Nosotros</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/contacto" onClick={closeMenu}>Contacto</Link></li>
       <li><CartWidget /></li>
     </ul>
   );
