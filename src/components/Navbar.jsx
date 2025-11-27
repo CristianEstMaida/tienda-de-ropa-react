14c8b863d573116c591e0938300082c4936ec9ce
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-26 22:30:11 -0300

    Import NavLinks component in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import CartWidget from './CartWidget'
 import NavbarResponsive from './NavbarResponsive'
 import AsideMenu from './AsideMenu'
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
   return (   
