8400d270a182f0ad5ba32fb53a67e46ccca79ed3
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-26 21:59:33 -0300

    Refactor Navbar component to include responsive and aside menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import CartWidget from './CartWidget'
+import NavbarResponsive from './NavbarResponsive'
+import AsideMenu from './AsideMenu'
 
 const Navbar = () => {
-  return (    
-      <aside>
-        <button className="close-menu" id="close-menu">
-          <i className="bi bi-x"></i>
-        </button>
-        <header className="navbar">
+  return (   
+    <>
+    <NavbarResponsive />
+      
+    <header className="navbar">
          <Link to="/" className="logo"><h1>Tienda de ropa</h1></Link>
-        </header>
+        
         <nav>
           <ul className="menu">
-              {/* <li><Link className="menu-link" to="/">Inicio</Link></li> */}
+              
               <li><Link className="menu-link boton-categoria" to="/productos">Productos</Link></li>
               <li><Link className="menu-link boton-categoria" to="/productos/medias">Medias</Link></li>
               <li><Link className="menu-link boton-categoria" to="/productos/pantalones">Pantalones</Link></li>
@@ -24,7 +25,9 @@ const Navbar = () => {
               <li><CartWidget /></li>
           </ul>
         </nav>
-      </aside>
+    </header>
+    <AsideMenu />
+    </>
   )
 }
 
