920da83ae4611b3b702a6010f83c04cf481f6cd9
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-26 23:02:48 -0300

    Replace closeMenu with handleClick in NavLinks

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 import CartWidget from './CartWidget';
 
 const NavLinks = ({ closeMenu }) => { 
+  const handleClick = () => {
+    if (closeMenu) {
+      closeMenu();
+    }
+  };
+
   return (
     <ul className="menu">
-      <li><Link className="menu-link boton-categoria" to="/productos" onClick={closeMenu}>Productos</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/medias" onClick={closeMenu}>Medias</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/pantalones" onClick={closeMenu}>Pantalones</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/remeras" onClick={closeMenu}>Remeras</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/abrigos" onClick={closeMenu}>Buzos</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/nosotros" onClick={closeMenu}>Nosotros</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/contacto" onClick={closeMenu}>Contacto</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos" onClick={handleClick}>Productos</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/medias" onClick={handleClick}>Medias</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/pantalones" onClick={handleClick}>Pantalones</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/remeras" onClick={handleClick}>Remeras</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/abrigos" onClick={handleClick}>Buzos</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/nosotros" onClick={handleClick}>Nosotros</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/contacto" onClick={handleClick}>Contacto</Link></li>
       <li><CartWidget /></li>
     </ul>
   );
