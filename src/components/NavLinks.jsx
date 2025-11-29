46223a24b739b825e2ecbfe87b8cfba483029f0e
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-29 15:20:29 -0300

    Update NavLinks.jsx

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -22,8 +22,8 @@ const NavLinks = ({ closeMenu }) => {
   return (
     <ul className="menu">
       <li><Link className="menu-link boton-categoria" to="/productos" onClick={handleClick}>Productos</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/calzadosfemeninos" onClick={handleClick}>Calzados femeninos</Link></li>
-      <li><Link className="menu-link boton-categoria" to="/productos/pantalonesfemeninos" onClick={handleClick}>Pantalones femeninos</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/calzados-femeninos" onClick={handleClick}>Calzados femeninos</Link></li>
+      <li><Link className="menu-link boton-categoria" to="/productos/pantalones-femeninos" onClick={handleClick}>Pantalones femeninos</Link></li>
       <li><Link className="menu-link boton-categoria" to="/productos/calzados" onClick={handleClick}>Calzados</Link></li>
       <li><Link className="menu-link boton-categoria" to="/productos/pantalones" onClick={handleClick}>Pantalones</Link></li>
       <li><Link className="menu-link boton-categoria" to="/productos/remeras" onClick={handleClick}>Remeras</Link></li>
