ead34438783c0b6ad4d5cd8a6aa19827466e62d1
Cristian Esteban Maida <55913935+CristianEstMaida@users.noreply.github.com>
2025-11-29 10:09:58 -0300

    Implement AuthContext for user authentication

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase/config'; 
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Suscribirse a los cambios en el estado de autenticación de Firebase
+        // Esto se ejecuta al inicio y cada vez que el usuario inicia o cierra sesión.
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        // Limpiar la suscripción al desmontar el componente
+        return () => unsubscribe();
+    }, []);
+
+    // El contexto proporciona el usuario autenticado y el estado de carga
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
